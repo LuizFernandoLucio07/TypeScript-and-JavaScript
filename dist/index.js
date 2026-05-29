0d9e80aedc6516a91811f3f6d736ab5288010fe1
LuizFernandoLucio07 <luizfernandoluciodev@gmail.com>
2026-05-29 12:10:38 -0300

    Correções e inclusão do gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function calcularArea(base, altura) {
-    return base * altura;
-}
-console.log('Area do retângulo:');
-console.log(calcularArea(10, 10));
-//# sourceMappingURL=index.js.map
