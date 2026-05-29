c1e48ea3833207ea0aa6da874989add1348bfbf3
LuizFernandoLucio07 <luizfernandoluciodev@gmail.com>
2026-05-29 11:51:49 -0300

    Meu primeiro commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function calcularArea(base, altura) {
+    return base * altura;
+}
+console.log('Area do retângulo:');
+console.log(calcularArea(10, 10));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function calcularArea(base: number, altura: number): number {
+  return base * altura;
+}
+
+console.log('Area do retângulo:');
+console.log(calcularArea(10, 10));
